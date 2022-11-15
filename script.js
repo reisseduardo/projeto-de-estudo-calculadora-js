8fbd74beef20c0bd2901a66dae5426bdfc655e1e
Eduardo Reis <116650399+reisseduardo@users.noreply.github.com>
2022-11-15 10:41:43 -0300

    script funcional aguardando ajustes finos

--- script.js
+++ script.js
@@ -4,6 +4,39 @@ const botao = document.querySelectorAll('.botao');
 botao.forEach((btn) => {
     btn.addEventListener("click", (e) => {
         const valor = e.target.innerText;
-            console.log(valor);
+            if(+valor >= 0 || valor === "." || valor === '+' || valor === '-' || valor === '*' || valor === '/' ){
+                insert(valor);}
+            if(valor === 'C'){
+                limpar();
+            }
+            if(valor === '='){
+                calcular(valor);
+            }
+            else{
+                console.log('Op ' + valor);
+            }
     })
 })
+
+ function insert(num)
+        {
+            var numero = display.innerHTML;
+            display.innerHTML = numero + num;
+        }
+
+function calcular()
+{
+    var resultado = display.innerHTML;
+    if(resultado)
+    {
+        display.innerHTML = eval(resultado);
+    }
+    else
+    {
+        display.innerHTML = "Nada..."
+    }
+}
+function limpar()
+    {
+        display.innerHTML = "";
+    }
