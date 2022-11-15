47f69651b166f3d92012115d43838571d59361ec
Eduardo Reis <116650399+reisseduardo@users.noreply.github.com>
2022-11-15 09:47:45 -0300

    reescrevendo variaveis e testando cada botao no console

--- script.js
+++ script.js
@@ -1,33 +1,9 @@
-const calculadora = document.querySelector(".calculadora");
-const botoes = calculadora.querySelector(".calculadora__botoes");
+const display = document.querySelector('.calculadora__display');
+const botao = document.querySelectorAll('.botao');
 
-botoes.addEventListener("click", e => {
-    if (e.target.matches("button")) {
-        // Fazer algo
-        const botao = e.target;
-        const action = botao.dataset.action;
-        if (!action) {
-            console.log('number key!')
-        }
-        if (
-            action === 'soma' ||
-            action === 'diminui' ||
-            action === 'multiplica' ||
-            action === 'divide'
-        ) {
-            console.log('operator key!')
-        }
-        if (action === 'ponto') {
-            console.log('decimal key!')
-        }
-
-        if (action === 'limpa') {
-            console.log('clear key!')
-        }
-
-        if (action === 'calcula') {
-            console.log('equal key!')
-        }
-    }
+botao.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        const valor = e.target.innerText;
+            console.log(valor);
+    })
 })
-
