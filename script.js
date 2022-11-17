ba8cb1b714b5ad21ba8a4092698607d3c982d75e
Eduardo Reis <116650399+reisseduardo@users.noreply.github.com>
2022-11-17 20:02:32 -0300

    reformulando o script para incluir o display oculto e corrigir o bug do ponto

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
+const displayOculto = document.querySelector('.display_oculto');
 const display = document.querySelector('.calculadora__display');
 const botao = document.querySelectorAll('.botao');
 
 botao.forEach((btn) => {
     btn.addEventListener("click", (e) => {
         const valor = e.target.innerText;
-
+        if (valor === '.') {
+            if (display.innerHTML.indexOf(".") === -1) {
+                insert(valor);
+            } else {
+                limpar();
+                alert('Use um operador válido!');
+            }
+        }
         if (+valor >= 0) {
             insert(valor);
         }
-        if (valor === "." || valor === '+' || valor === '-' || valor === '*' || valor === '/') {
+        if (valor === '+' || valor === '-' || valor === '*' || valor === '/') {
             let ultimoDigito = display.innerHTML.slice(-1);
-            if (ultimoDigito === '' || ultimoDigito === '.' || ultimoDigito === '+' || ultimoDigito === '-' || ultimoDigito === '*' || ultimoDigito === '/') {
+            if (ultimoDigito === '' || ultimoDigito === '+' || ultimoDigito === '-' || ultimoDigito === '*' || ultimoDigito === '/') {
                 limpar();
-                alert('Use um operador válido!')
+                alert('Use um operador válido!');
+
             } else {
                 insert(valor);
+                displayOculto.innerHTML = display.innerHTML;
+                limpar();
             }
         }
         if (valor === 'C') {
             limpar();
         }
         if (valor === '=') {
+            displayOculto.innerHTML = displayOculto.innerHTML + display.innerHTML;
             calcular(valor);
+            display.innerHTML = displayOculto.innerHTML;
         }
 
     })
@@ -33,9 +46,9 @@ function insert(num) {
 }
 
 function calcular() {
-    var resultado = display.innerHTML;
+    var resultado = displayOculto.innerHTML;
     if (resultado) {
-        display.innerHTML = eval(resultado);
+        displayOculto.innerHTML = eval(resultado);
     }
     else {
         display.innerHTML = "VAZIO";
